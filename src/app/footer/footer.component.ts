36aa3f9d1d5ae6027fb925e9f40ebd9ddb0623ff
Alexandra Antal <antal.alexandra98@gmail.com>
2020-03-23 20:10:00 +0200

    Footer icons

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { faFacebookSquare, faInstagramSquare } from '@fortawesome/free-brands-svg-icons';
+import { faPhoneSquare, faEnvelopeSquare } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+
+  faFacebook = faFacebookSquare;
+  faInstagram = faInstagramSquare;
+  faPhone = faPhoneSquare;
+  faEnvelope = faEnvelopeSquare;
 
   constructor() { }
 
